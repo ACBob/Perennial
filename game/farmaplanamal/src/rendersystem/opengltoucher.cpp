fd2c336daddc62285cf4383e78c6e16b4f785ea4
ACBob <ethbot@hotmail.com>
2019-09-30 20:16:19 +0100

    Remove un-used Shader Source variables.

--- game/farmaplanamal/src/rendersystem/opengltoucher.cpp
+++ game/farmaplanamal/src/rendersystem/opengltoucher.cpp
@@ -56,22 +56,6 @@ namespace perennial{
                 glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //Don't worry, Apple is UNIX so we will do this.
             #endif
 
-            const char *vertexShaderSource = 
-                "#version 450 core\n"
-                "layout (location = 0) in vec3 aPos;\n"
-                "void main()\n"
-                "{\n"
-                    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-                "}\n";
-
-            const char *fragmentShaderSource =
-                "#version 450 core\n"
-                "out vec4 FragColor;\n"
-                "void main()\n"
-                "{\n"
-                    "FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
-                "}\n";
-
             printf("perennial::render::create_window\n");
             GLFWwindow* GameWindow = glfwCreateWindow(800, 600, "Perennial Window", NULL, NULL);
             perennial::rendering::GameWindow = GameWindow;
