abcb631bffc09dc9f8743fe45c213722076ff227
ACBob <ethbot@hotmail.com>
2019-10-02 23:11:57 +0100

    Texture!

---
+++ game/farmaplanamal/src/rendersystem/textures/texturesystem.cpp
@@ -0,0 +1,27 @@
+#include "include/stb/stb_image.h"
+#include "glad/glad.h"
+#include "texturesystem.h"
+namespace perennial{
+    namespace rendering{
+        namespace textures{
+            int LoadImage(char* FilePath){
+                int width, height, nrChannels;
+                unsigned char *data = stbi_load(FilePath, &width, &height, &nrChannels, 0); 
+                stbi_image_free(data);
+                return width, height, nrChannels;
+            }
+            GLuint MakeTexture(char* FilePath){
+                
+                int width, height, nrChannels = LoadImage(FilePath);
+                unsigned int texture;
+                glGenTextures(1, &texture);
+
+                glBindTexture(GL_TEXTURE_2D, texture);  
+
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+                glGenerateMipmap(GL_TEXTURE_2D);
+
+            }
+        }
+    }
+}
---
+++ game/farmaplanamal/src/rendersystem/textures/texturesystem.h
@@ -0,0 +1,9 @@
+#include "include/stb/stb_image.h"
+#include "glad/glad.h"
+namespace perennial{
+    namespace rendering{
+        namespace textures{
+            GLuint MakeTexture(char* FilePath);
+        }
+    }
+}
