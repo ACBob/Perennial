1943022e8a22911e7748a591976b80fe95ec3828
ACBob <ethbot@hotmail.com>
2019-10-19 01:07:02 +0100

    Accidently commit this, So i may as well commit the "finished" one.

--- projects/engine/src/random/perlin.cpp
+++ projects/engine/src/random/perlin.cpp
@@ -1,5 +1,37 @@
+namespace perennial{
+    namespace randomgen{
+        namespace perlinlike{
+            //3 Layers, fancy!
+            
+            //PURPOSE: Generate a 1D array of Perlin-like Noise.
+            //Taken from https://www.youtube.com/watch?v=6-0UaeJBumA , but with different variable names.
+            float *f_generatePerlin1D(int iOutputSize, float *fSeed, int iOctaves)
+            {
+                float *fOutput;
+                for(int iX = 0; iX < iOutputSize; iX++)
+                {
+                    float fNoise = 0.0f;
+                    float fScale = 1.0f;
+                    float fScaleAcc = 0.0f;
 
-float *r_NoisePerlin1D;
-float *r_NoisePerlin2D;
+                    for (int iO = 0; iO < iOctaves; iO++)
+                    {
+                        int iPitch = iOutputSize >> iO;
+                        int iSample1 = (iX / iPitch) * iPitch;
+                        int iSample2 = (iSample1 + iPitch) % iOutputSize;
 
+                        float fBlend = (float)(iX - iSample1) / (float)iPitch;
+                        float fSample = (1.0f - fBlend) * fSeed[iSample1] + fBlend * fSeed[iSample2];
+                        fNoise += fSample * fScale;
 
+                        fScaleAcc += fScale;
+                        fScale = fScale/2.0f;
+                    }
+                    fOutput[iX] = fNoise / fScaleAcc;
+                }
+
+                return fOutput;
+            }
+        }
+    }
+}
