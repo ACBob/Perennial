f74f000ba9eae818a99c858da94fc8e48efc2f1d
ACBob <ethbot@hotmail.com>
2019-11-21 21:37:20 +0000

    We're redoing it all

---
+++ projects/engine/src/p_main.cpp
@@ -0,0 +1,16 @@
+//-- Perennial File
+//--- Purpose: Define entry points
+
+//Includes
+#include <cstdio.h>
+
+// p_main
+//- Returns INT
+//- Entry Point
+int PerennialMain()
+{
+
+	//Should we hard-code version number?
+	sprintf("Perennial (V0.0)");
+
+}
